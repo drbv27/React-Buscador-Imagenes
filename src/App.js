e38db158363ec6bfa3de03eb2c5a491eb6925862
Diego <drbv27@gmail.com>
2022-01-08 10:22:02 -0500

    Iterate the images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./header.css";
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
+  const open = (url) => {
+    window.open(url);
+  };
   console.log({ photos });
   return (
     <div>
@@ -33,6 +36,16 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.id} />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
